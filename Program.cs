8c9e44f99f0fe762dceb9f2601efb61d99e8ec04
RaminIsko <ramsmagast@gmail.com>
2023-03-21 00:15:34 +0600

    Второй коммит - создание метода конвертированного массива строк

--- Program.cs
+++ Program.cs
@@ -12,3 +12,15 @@ string[] GetArray (){
 
     return result;
 }
+
+// 2 коммит - создание Метода получения конвертированного массива строк
+
+string ConvertedArray (string[] array){
+    
+    string result = "";
+    
+    for(int i = 0; i < array.Length; i++) if(array[i].Length <= 3) result += $"{array[i]} ";
+
+    return result;
+
+}
