4614c2e95afd1122e40439d3348d09aeaa7e2a14
RaminIsko <ramsmagast@gmail.com>
2023-03-20 23:43:29 +0600

    Первый коммит - Метод создания массива строк

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+
+// 1 коммит - создание Метода получения массива строк
+
+string[] GetArray (){
+    
+    string element = ReadLine()!;
+
+    string[] result = element.Split(new char[] {' '});
+
+    return result;
+}
